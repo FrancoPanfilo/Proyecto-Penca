13da194b0a52124a115f139f0d3cb01341fac785
FrancoPanfilo <108556532+FrancoPanfilo@users.noreply.github.com>
2022-08-12 20:17:36 -0300

    pa mi ya esta el js

--- PF1/CrearPenca/CP.js
+++ PF1/CrearPenca/CP.js
@@ -1,7 +1,6 @@
 //identificador Penca
 let identificador = document.getElementById("identificador")
 let botonFijar = document.getElementById("fijarId")
-let idDisponible = false
 // equipos
 let equipo1 = document.getElementById("equipo1")
 let equipo2 = document.getElementById("equipo2")
@@ -10,16 +9,89 @@ let crearPartido = document.getElementById("crearPartido")
 // Lista pencas
 let pencasExistentes = JSON.parse(localStorage.getItem("Pencas Predefinidas"))
 
+// La penca que se va a crear
+
+let nuevaPenca 
+let idDisponible
+
+// tabla que se va a modificar
+
+let tabla = document.getElementById("Fixture")
+let titulo = document.getElementById("tituloTabla")
+
 botonFijar.addEventListener("click",()=>{
-    for (let i=0;i<pencasExistentes.pencas.length;i++){
-        if ((pencasExistentes.pencas.identificador==identificador.value)||(identificador.value=="")){
+    let i=0
+    idDisponible = true
+    while ((i<pencasExistentes.pencas.length)&&(idDisponible==true)){
+        let id=identificador.value
+        if ((pencasExistentes.pencas[i].identificador==id)||(id=="")){
             idDisponible=false
-        }else{
-            idDisponible=true
+        }
+        i=i+1
+    }
+    if (idDisponible==true){
             let idPenca=identificador.value
             let idMostrar = document.getElementById("idMostrar")
             idMostrar.innerHTML=`<br><h3>${idPenca}</h3>`
             botonFijar.className="Esconder"
+            nuevaPenca = new Penca(idPenca,(JSON.parse(localStorage.getItem("Usuario"))).id)
+            titulo.innerHTML=`<h3>${idPenca}</h3>`
+    }else{
+        if (identificador.value==""){
+            alert("Ingrese un identificador para su penca")
+        }else{
+            alert("Identificador en uso")
         }
+        identificador.value=""
+    }
+})
+
+// Agregar partido
+
+crearPartido.addEventListener("click",()=>{
+    if (idDisponible){
+        let team1 = equipo1.value
+    let team2 = equipo2.value
+    if (team2==team1){
+        alert("Los equipos no pueden ser iguales")
+    }else if ((team1=="")||(team2=="")){
+        alert("Ingrese el nombre de los equipos")
+    }else{
+        nuevaPenca.IngresarPartido(team1,team2)
+        alert("El partido se asigno correctamente")
+        equipo1.value=""
+        equipo2.value=""
+        // necesito imprimir esos datos en la tabla
+        let u = nuevaPenca.fixture.length -1
+        ultimoPartido = nuevaPenca.fixture[u]
+        let agregarElemento = document.createElement("tr")
+        agregarElemento.innerHTML= `<tr>
+                                        <td>${u+1}</td>
+                                        <td>${ultimoPartido.team1}</td>
+                                        <td>${ultimoPartido.goles1}</td>
+                                        <td>VS</td>
+                                        <td>${ultimoPartido.goles2}</td>
+                                        <td>${ultimoPartido.team2}</td>
+                                    </tr>`
+        tabla.appendChild(agregarElemento)
+    }
+    }else {
+        alert("Fije el identificador de la Penca")
+    }
+    
+})
+
+// Crear la penca
+
+let crearPenca = document.getElementById("crearPenca")
+
+crearPenca.addEventListener("click",()=>{
+    console.log(nuevaPenca);
+    if (nuevaPenca.fixture.length<6){
+        alert("Agregue al menos 6 partidos")
+    }else{
+        pencasExistentes.pencas.push(nuevaPenca)
+        localStorage.setItem("Pencas Predefinidas",JSON.stringify(pencasExistentes))
+        location.reload()
     }
 })
